fb8a6720d3d5628c9c1f98ad7ee889bca476ddb9
freekong <daijhwq@163.com>
2022-09-26 17:20:48 +0800

    实现解析element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast"
 
+const enum tagType {
+  Start,
+  End
+}
+
 export function baseParse(content) {
 
   const context = createParserContext(content)
@@ -36,13 +41,43 @@ function parseChildren(context) {
   const nodes: any = [];
 
   let node
-  if (context.source.startsWith('{{')) {
+  const s = context.source
+  if (s.startsWith('{{')) {
     node = parseInterpolation(context)
+  } else if (s[0] == '<') {
+    if (/[a-z]/i.test(s[1])) {
+      console.log('[ parse element ]')
+      node = parseElement(context)
+    }
   }
   nodes.push(node)
   return nodes
 }
 
+function parseElement(context) {
+
+  const element = parseTag(context, tagType.Start);
+
+  parseTag(context, tagType.End)
+  console.log('[ context.source ] ============', context.source)
+  return element
+  
+}
+
+function parseTag(context, type: tagType) {
+  // 1.解析tag
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  console.log('[ match ] >', match)
+  const tag = match[1]
+  // 2.删除字符
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+  if (type === tagType.End) return
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
+}
 function createParserContext(content: any) {
   return {
     source: content
--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -17,4 +17,16 @@ describe('parse', () => {
       })
     })
   })
+
+  describe('element', () => {
+    test('simple-element',() => {
+
+      const ast = baseParse("<div></div>")
+
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: 'div'
+      })
+    })
+  })
 })

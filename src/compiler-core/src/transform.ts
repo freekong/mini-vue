f064886ae69e5f85b16bac7f1526876bf257c932
freekong <17767239286@163.com>
2022-10-03 21:15:30 +0800

    实现transform功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,35 @@
+export function transform(root, options) {
+  const context = createTransformContext(root, options);
+  // 1.遍历树  深度优先
+  traverseNode(root, context);
+}
+
+function traverseNode(node, context) {
+  console.log(node, "=====>node");
+  const nodeTransforms = context.nodeTransforms;
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i];
+    transform(node);
+  }
+
+  traverseChildren(node, context);
+}
+
+function traverseChildren(node: any, context: any) {
+  const children = node.children;
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i];
+      traverseNode(node, context);
+    }
+  }
+}
+
+function createTransformContext(root: any, options: any) {
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || [],
+  };
+
+  return context;
+}
